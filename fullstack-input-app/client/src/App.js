ea708979fa5dbcfae1e6e627b703302639af17a3
marcandy <mnoeljeu@jcp.com>
2019-01-15 11:35:33 -0600

    fullstack-input-app frontend setup

---
+++ fullstack-input-app/client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>I'm Ready to use tehe backend API.</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
