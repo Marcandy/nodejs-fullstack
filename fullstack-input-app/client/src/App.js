cc302631342eed09c31d946ac7f955c59debdd76
marcandy <mnoeljeu@jcp.com>
2019-01-17 10:49:35 -0600

    full stack update

--- fullstack-input-app/client/src/App.js
+++ fullstack-input-app/client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { url } from 'inspector';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -23,6 +25,45 @@ class App extends Component {
 
   componentWillUnmount() {
   }
+  // get Data from api
+  getDataFromDb = () => {
+    fetch("/api/getData")
+      .then(data => data.json())
+      .then(res => this.setState({ data: res.data }));
+  }
+
+  // put and update data
+  putDataDB = (message) => {
+    // map through ids
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+
+    while(currentIds.includes(idToBeAdded)) {
+      ++idToBeAdded;
+    }
+
+    axios.post('/api/putData', {
+      id: idToBeAdded,
+      message: message
+    })
+  }
+
+  deleteFromDB = (idToDelete) => {
+    let objIdToDelete = null;
+
+    this.state.data.forEach(dat => {
+      if( dat.id = idToDelete){
+        objIdToDelete = dat._id;
+      }
+    });
+
+    axios.delete('/api/deleteData', {
+      data: {
+        id: objIdToDelete,
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
