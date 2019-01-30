57464d258aff4e068cdbb97bea72fb3eeff75baa
marcandy <mnoeljeu@jcp.com>
2019-01-30 14:46:40 -0600

    controller article endpoint

--- medium-fullstack/server/controllers/article.ctrl.js
+++ medium-fullstack/server/controllers/article.ctrl.js
@@ -41,5 +41,49 @@ module.exports = {
                 next();
             })
         }
+    },
+    getAll: (req, res, next) => {
+        Article.find(req.params.id)
+        .populate('author')
+        .populate('comments.author').exec((err, article)=> {
+            if (err)
+                res.send(err)
+            else if (!article)
+                res.send(404)
+            else
+                res.send(article)
+            next();
+        })
+    },
+    clapArticle: (req, res, next) => {
+        Article.findById(req.body.article_id).then((article)=> {
+            return article.clap().then(()=>{
+                return res.json({msg: "Done"})
+            })
+        }).catch(next)
+    },
+
+    commentArticle: (req, res, next) => {
+        Article.findById(req.body.article_id).then((article)=> {
+            return article.comment({
+                author: req.body.author_id,
+                text: req.body.comment
+            }).then(() => {
+                return res.json({msg: "Done"})
+            })
+        }).catch(next)
+    },
+    getArticle: (req, res, next) => {
+        Article.findById(req.params.id)
+        .populate('author')
+        .populate('comments.author').exec((err, article)=> {
+            if (err)
+                res.send(err)
+            else if (!article)
+                res.send(404)
+            else
+                res.send(article)
+            next();
+        })
     }
 };
